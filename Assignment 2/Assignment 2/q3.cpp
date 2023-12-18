9e8dc1795fcf2eead8ddc7f10a0104327fe46a20
Uddhav Pawar <93379977+Uddhav2404@users.noreply.github.com>
2023-12-18 10:50:12 +0530

    Add files via upload

---
+++ Assignment 2/Assignment 2/q3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two nos:";
+    cin>>a>>b;
+    cout<<"Sum is "<<a+b;
+    return 0;
+}
---
+++ Assignment 2/Assignment 2/q5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,h;
+    cout<<"Enter length ,breath and height:";
+    cin>>l>>b>>h;
+    cout<<"Volume is "<<l*b*h;
+    return 0;
+}
